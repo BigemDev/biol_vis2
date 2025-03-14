3b831c624256dcf02fe8b7c4b19cea2b2883fd16
bigemdev@proton.me <bigemdev@proton.me>
2025-03-14 22:43:27 +0100

    LGTM

--- obj/Debug/net9.0/projekt2.AssemblyInfo.cs
+++ obj/Debug/net9.0/projekt2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad32a3528899971920d3a4f652ac45e11aee7b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec71523236468742db062fba7afaa3fdc35d543")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
